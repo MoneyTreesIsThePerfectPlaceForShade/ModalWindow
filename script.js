9f4ccce068182a745106504b9fae7c4a91a780d3
Money Trees <matveyshakur@gmail.com>
2022-09-25 20:44:41 +0500

    init: started project

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// берем всё, что нам нужно
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    console.log(btnsOpenModal[i].textContent);
+}
