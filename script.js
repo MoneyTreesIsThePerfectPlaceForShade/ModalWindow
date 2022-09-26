70b08abecbbf889359d015eebae5ddc85a3d6ce0
Money Trees <matveyshakur@gmail.com>
2022-09-26 11:42:00 +0500

    feat:added some fun: now you can open modal window and close it

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
+// применяем принцип DRY
+const closeModalWindow = () => {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+const openModalWindow = () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener("click", openModalWindow);
 }
+
+// заметь, что метод передается без скобок, то бишь не вызываем его
+// мы лишь декларируем метод, а когда происходит клик
+// JS его самостоятельно вызывает
+btnCloseModal.addEventListener("click", closeModalWindow);
+overlay.addEventListener("click", closeModalWindow);
